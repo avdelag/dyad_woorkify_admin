cf240e2368a7bbc0c7c76a514e97fab89a580581
[dyad] <git@dyad.sh>
2025-05-09 09:53:01 -0500

    [dyad] wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/auth/Login'
+import Signup from './pages/auth/Signup'
+import AdminSecret from './pages/auth/AdminSecret'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/auth/login" element={<Login />} />
+      <Route path="/auth/signup" element={<Signup />} />
+      <Route path="/auth/admin-secret" element={<AdminSecret />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
